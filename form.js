c2c5dd69d241c3d47db01a480430a72910f6556d
Kacper Adamski <kacper.adamski95@gmail.com>
2022-04-08 21:11:30 +0200

    added some coments

--- form.js
+++ form.js
@@ -103,28 +103,32 @@ window.addEventListener('DOMContentLoaded', (event) => {
     //         console.log(i);
     //     }
     // }
-    /* #5 — Iterate through all numbers from 1 to 45. Print the following:
-    For multiples of 3 print “Fizz”
-    For multiples of 5 print “Buzz”
-    For multiples of 3 and 5 print “FizzBuzz”
-    */
-//    for (var i = 1; i <= 45; i++) {
-//     if (i % 3 == 0 && i % 5 == 0) {
-//     console.log("FizzBuzz");
-//     } else if (i % 3 == 0) {
-//     console.log("Fizz");
-//     } else if (i % 5 == 0) {
-//     console.log("Buzz");
+    // #5 — Iterate through all numbers from 1 to 45. Print the following:
+    // For multiples of 3 print “Fizz”
+    // For multiples of 5 print “Buzz”
+    // For multiples of 3 and 5 print “FizzBuzz”
+    // Warto zapamiętać ten przykład, bo może pojawić się na rozmowie kwalifikacyjnej
+    for (var index = 1; index <= 45; index++) {
+        if (index % 3 == 0 && index % 5 == 0) {
+        console.log("FizzBuzz");
+        } else if (index % 3 == 0) {
+        console.log("Fizz");
+        } else if (index % 5 == 0) {
+     console.log("Buzz");
+    });
+     // && - i 
+     // || - lub (or)
+     // ^ - to lub to (jeden z, ale nie 2 równocześnie, musi być więcej niż 2 zmienne)
+     // ! - negacja (!=)
 //     }
-// }
+//     }
     // let thisIsAnArray = ["element1", "element2", "element3", "element4"];
     // for (let i = 0; i < thisIsAnArray.length; i++) {
     //     console.log(thisIsAnArray[i]);
     // }
-    let numberArrays = [7,10,153,210,14];
-    let sum = 0;
-    for (let i = 0; i < numberArrays.length; i++) {
-        sum = sum + numberArrays[i];
-    }
-    console.log("The sum is: " + sum);
-});
+    // let numberArrays = [7,10,153,210,14];
+    // let sum = 0;
+    // for (let i = 0; i < numberArrays.length; i++) {
+    //     sum = sum + numberArrays[i];
+    // }
+    // console.log("The sum is: " + sum);
